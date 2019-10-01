2d616639984e6c83ace18f3c3857b097dae2e131
bag-PacKer <35328339+bag-PacKer@users.noreply.github.com>
2019-10-01 20:28:34 +0530

    Create debug_template.cpp

---
+++ languages/debug_template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+//STL shortcuts
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define F first
+#define S second
+#define MP make_pair
+ 
+// Error detection for debug code
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+ 
+using namespace std;
+ 
+typedef long long int ll; // 64 bit integer
+typedef unsigned long long int  ull; // 64 bit unsigned integer
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<int,ll> pill;
+ 
+int main()
+{
+    //Your code goes here
+ 
+ 
+  return 0;
+}
